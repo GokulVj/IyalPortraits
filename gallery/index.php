99f0449c6b2589ef5b861df90db2dab91a04c5d3
Gokul Srinivasan <srigokul@amazon.com>
2019-03-02 03:03:38 +0530

    Adding Gallery Page - Just Static Images

---
+++ gallery/index.php
@@ -0,0 +1,170 @@
+<html lang="en">
+    <head>
+        <?php
+            $title = "Gallery";
+            require '../header.php';
+        ?>
+        <link href="../css/full-slider.css" rel="stylesheet">
+        <link href="../css/Icomoon/style.css" rel="stylesheet" type="text/css" />
+        <link href="../css/animated-masonry-gallery.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="../css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="../css/component.css" />
+        <link href="../css/lightbox.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="../js/modernizr.min.js"></script>
+    </head>
+    <body>
+        <?php require '../navbar.php'; ?>
+
+        <div class="canvas gallery">
+            <h1 class="blog-post-title text-center">Gallery of Images</h1>
+            <span class="title-divider"></span>
+            <section id="photostack-3" class="photostack">
+                <div>
+                    <figure>
+                        <a href="../img/a1.jpg" data-lightbox="studio2" class="photostack-img"><img src="../img/a1.jpg" height="240px" width="240px" alt="img05"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Wedding Photography</h2>
+                            <div class="photostack-back">
+                                <ul>
+                                    <li>Wedding</li>
+                                    <li>Engagement</li>
+                                    <li>Reception</li>
+                                    <li>Mehendi</li>
+                                    <li>Sangeeth</li>
+                                </ul>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/b1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/b1.jpg" height="240px" width="240px" alt="img02"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Outdoor Shoots</h2>
+                            <div class="photostack-back">
+                                <p>These Happy Days are yours and mine Happy Days. It's a beautiful day in this neighborhood a beautiful day for a neighbor. Would you be mine?</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/c1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/c1.jpg" height="240px" width="240px" alt="img03"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Commercial Wear Shoots</h2>
+                            <div class="photostack-back">
+                                <p>It's a neighborly day in this beautywood a neighborly day for a beauty. Would you be mine? Could you be mine. Now the world don't move to the beat of just one drum.</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/d1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/d1.jpg" height="240px" width="240px" alt="img01"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Post Wedding Shoots</h2>
+                            <div class="photostack-back">
+                                <p>I have never been to a place so serene in my entire life before. Swimming in clear waters opened my mind to nature and reminded me of my and <span>eveybody</span> everybody else's mortality.</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/e1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/e1.jpg" height="240px" width="240px" alt="img04"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">DJ - MC with Anchor</h2>
+                            <div class="photostack-back">
+                                <p>Engagement - Reception - Wedding - Mehendi - Sangeeth</p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/f1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/f1.jpg" height="240px" width="240px" alt="img06"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Pre Wedding Shoots</h2>
+                            <div class="photostack-back">
+                                <p>Fish don't fry in the kitchen and beans don't burn on the grill. Took a whole lotta tryin' just to get up that hill. Baby if you've ever wondered - wondered whatever became of me. </p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure>
+                        <a href="../img/g1.jpg"  data-lightbox="studio2" class="photostack-img"><img src="../img/g1.jpg" height="240px" width="240px" alt="img06"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Baby Shoots</h2>
+                            <div class="photostack-back">
+                                <p>Fish don't fry in the kitchen and beans don't burn on the grill. Took a whole lotta tryin' just to get up that hill. Baby if you've ever wondered - wondered whatever became of me. </p>
+                            </div>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/7.jpg" height="240px" width="240px" alt="img07"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Engagement</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/8.jpg" height="240px" width="240px" alt="img08"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Reception</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/9.jpg" height="240px" width="240px" alt="img09"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Model Shoots</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/10.jpg" height="240px" width="240px" alt="img10"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Outdoor Shoots</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/11.jpg" height="240px" width="240px" alt="img11"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Sangeeth</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/12.jpg" height="240px" width="240px" alt="img12"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Industraial Shoots</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/13.jpg" height="240px" width="240px" alt="img13"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Ad Flims</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/14.jpg" height="240px" width="240px" alt="img14"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Puberty Shoots</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/15.jpg" height="240px" width="240px" alt="img15"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Fashion Shoots</h2>
+                        </figcaption>
+                    </figure>
+                    <figure data-dummy>
+                        <a href="#" class="photostack-img"><img src="../img/16.jpg" height="240px" width="240px" alt="img16"/></a>
+                        <figcaption>
+                            <h2 class="photostack-title">Maternity Shoots</h2>
+                        </figcaption>
+                    </figure>
+                </div>
+            </section>
+        </div>
+
+        <?php require '../footer.php' ?>
+        <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.min.js" integrity="sha256-oTyWrNiP6Qftu4vs2g0RPCKr3g1a6QTlITNgoebxRc4=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="../js/isotope.js"></script>
+        <script type="text/javascript" src="../js/animated-masonry-gallery.js"></script>
+        <script src="../js/classie.js"></script>
+        <script src="../js/photostack.js"></script>
+        <script src="../js/lightbox.js"></script>
+        <script>
+            $(document).ready(function(){
+                new Photostack( document.getElementById( 'photostack-3' ));
+            });
+        </script>
+    </body>
+</html>
